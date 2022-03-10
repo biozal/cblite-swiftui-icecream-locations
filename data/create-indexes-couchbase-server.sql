f8832a1b0bcfc99774630437f0fbd0539336c511
Aaron LaBeau <alabeau@gmail.com>
2022-03-10 08:35:25 -0600

    updates to the mobile app for indexing

--- data/create-indexes-couchbase-server.sql
+++ data/create-indexes-couchbase-server.sql
@@ -1,3 +1,7 @@
 CREATE PRIMARY INDEX icecream
-CREATE INDEX icecream_state_idx ON icecream(properties.`addr:state`)
+
+CREATE INDEX adv_properties_name ON `icecream`(`properties`.`name`)
+
+CREATE INDEX adv_properties_name_properties_addrcity ON `icecream`(`properties`.`name`,`properties`.`addr:city`)
+
 CREATE INDEX icecream_state_city_idx ON icecream(properties.`addr:state`, properties.`addr:city`)
